948e759ad5e506cc1c908ba157ef59c8e4753ac5
makimaki1005 <gumdam1999r@gmail.com>
2019-10-29 01:07:57 +0900

    playerとゴールの当たり判定とcsv書き換え

--- MusicGame/Actor/Player.cs
+++ MusicGame/Actor/Player.cs
@@ -164,56 +164,52 @@ namespace MusicGame.Actor
                             switch (gameObject.dir)
                             {
                                 case Direction.Top:
-                                    if (position.Y < Pos.Y && position.X > Pos.X - 20 && position.X < Pos.X + 126)
+                                    if (position.Y < Pos.Y && position.X > Pos.X - 48 && position.X < Pos.X + 48)
                                     {
                                         reset = true;
                                         stop = !stop;
                                         _hit = true;
                                         position = new Vector2(gameObject.GetPosition().X + 16,
                                         gameObject.GetPosition().Y + 16);
+                                        StageState.isClear = true;
                                     }
                                     break;
                                 case Direction.Bottom:
-                                    if (position.Y > Pos.Y + 48 && position.X > Pos.X - 20 && position.X - 35 < Pos.X + 126)
+                                    if (position.Y > Pos.Y + 30 && position.X > Pos.X - 48 && position.X < Pos.X + 48)
                                     {
                                         reset = true;
                                         stop = !stop;
                                         _hit = true;
                                         position = new Vector2(gameObject.GetPosition().X + 16,
                                         gameObject.GetPosition().Y + 16);
+                                        StageState.isClear = true;
                                     }
                                     break;
                                 case Direction.Left:
-                                    if (position.X < Pos.X && position.Y > Pos.Y - 20 && position.Y < Pos.Y + 126)
+                                    if (position.X < Pos.X && position.Y > Pos.Y - 35 && position.Y < Pos.Y + 96)
                                     {
                                         reset = true;
                                         stop = !stop;
                                         _hit = true;
                                         position = new Vector2(gameObject.GetPosition().X + 16,
                                         gameObject.GetPosition().Y + 16);
+                                        StageState.isClear = true;
                                     }
 
                                     break;
                                 case Direction.Right:
-                                    if (position.X > Pos.X + 20 && position.Y > Pos.Y - 20 && position.Y < Pos.Y + 126)
+                                    if (position.X > Pos.X + 48 && position.Y > Pos.Y - 48 && position.Y < Pos.Y + 48)
                                     {
                                         reset = true;
                                         stop = !stop;
                                         _hit = true;
                                         position = new Vector2(gameObject.GetPosition().X + 16,
                                         gameObject.GetPosition().Y + 16);
+                                        StageState.isClear = true;
                                     }
-                                    break;
-                                case Direction.Free:
-                                    reset = true;
-                                    stop = !stop;
-                                    _hit = true;
-                                    position = new Vector2(gameObject.GetPosition().X + 16,
-                                    gameObject.GetPosition().Y + 16);
                                     break;
                             }
 
-                            StageState.isClear = true;
                         }
                     }
                 }
